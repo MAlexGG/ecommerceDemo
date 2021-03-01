c66e49fa877e773aff0db4e355142a4ca82e0b0e
MAlexGG <alexandragalarzag@gmail.com>
2021-03-01 11:49:48 +0100

    versionAlex

--- cambiarPantalla.js
+++ cambiarPantalla.js
@@ -1,30 +1,32 @@
 let body1 = document.querySelector(".body1");
 let body2 = document.querySelector(".body2");
 
-function cambiarPantalla(){
-    
-    if(body1.style.display == "block"){
+function cambiarPantalla() {
+
+    if (body1.style.display == "block") {
         body1.style.display = "none"
     }
-    if(body2.style.display == "none"){
+    if (body2.style.display == "none") {
         body2.style.display = "block"
     }
     console.log(pedido);
 }
 
-function retorno(){
-    
-    if(body2.style.display == "block"){
+function retorno() {
+
+    if (body2.style.display == "block") {
         body2.style.display = "none"
     }
-    if(body1.style.display == "none"){
-        body1.style.display = "block"; 
+    if (body1.style.display == "none") {
+        body1.style.display = "block";
     }
 
     dishes2 = "";
     pintarMenu();
     console.log(carrito);
     console.log(listaPlatos);
-    
-}
 
+    let catClick = document.querySelectorAll(".contenedorCategoriasClick");
+    catClick.classList.remove("contenedorCategoriasClick");
+
+}
